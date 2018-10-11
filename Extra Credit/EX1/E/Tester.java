84dfb3ed1a9976ddd8d980c10edf5d8b7b6506c0
Sunny Vaswani <nerfsunny@gmail.com>
2018-10-10 22:46:53 -0400

    Finished Tester. Implemented methods to find the necessary strings as well as improve old code.

--- Extra Credit/EX1/E/Tester.java
+++ Extra Credit/EX1/E/Tester.java
@@ -5,27 +5,73 @@ import java.util.*;
 
 public class Tester
 {
-	public static int numberOfAthletesInjured(HashMap<String, Athlete> list)
+	public static String GetAthlete(HashMap<String, Athlete> list, String id)
 	{
-		int count = 0;
+		String athlete = null;
+
+		if(list.containsKey(id) == false)
+		{
+			return "There is no Athlete that has that Athlete ID";
+		}
 
 		for(Athlete x : list.values())
 		{
-			if(x.getCurrentlyInjured() == true)
+			if(x.getAthleteID().equalsIgnoreCase(id))
 			{
-				count++;
+				if(x.getAge() < 20)
+				{
+					System.out.println("Athlete " + x.getAthleteName() + " is underaged." + " Age: " + x.getAge());
+					athlete = x.toString();
+				}else
+				{
+					athlete = x.toString();
+				}
 			}
 		}
 
-		return count;
+		return athlete;
+	}
+
+	public static double getSalary(HashMap<String, Athlete> list, String team, int age)
+	{
+		double totalSalary = 0.0;
+
+		for(Athlete x : list.values())
+		{
+			if((x.getAge() >= age) && (x.getAge() < age + 10) && (x.getTeam().equalsIgnoreCase(team)))
+			{
+				totalSalary = totalSalary + x.getSalary();
+			}
+		}
+
+		return totalSalary;
+	}
+
+	public static void teamAndState(HashMap<String, Athlete> list, String team, String birth)
+	{
+		for(Athlete x : list.values())
+		{
+			if(x.getTeam().equalsIgnoreCase(team) && x.getAthleteBornIn().equalsIgnoreCase(birth))
+			{
+				if(x.getAge() < 20)
+				{
+					System.out.println("Athlete " + x.getAthleteName() + " is underaged." + " Age: " + x.getAge());
+					System.out.println(x.toString());
+				}else
+				{
+					System.out.println(x.toString());
+				}
+			}
+		}
 	}
 
 	public static void main(String args[])
 	{
 		HashMap<String, Athlete> listOfAthletes = new HashMap<String, Athlete>();
-		HashMap<String, Athlete> listOfDuplicateAthletes = new HashMap<String, Athlete>();
 		Athlete person = null;
 		RandomAccessFile raf = null;
+		int injuredPlayers = 0;
+		String specificID = "85da6bad-48bd-4bd8-85bb-61970e1f53eb";
 
 		try
 		{
@@ -35,10 +81,22 @@ public class Tester
 			{
 				String line = raf.readLine();
 				person = new Athlete(line);
+
+				if(person.getCurrentlyInjured() == true)
+				{
+					injuredPlayers++;
+				}
 				
 				if(listOfAthletes.containsKey(person.getAthleteID()) == true)
 				{
-					listOfDuplicateAthletes.put(person.getAthleteID(), person);
+					if(person.getAge() < 20)
+					{
+						System.out.println("Athlete " + person.getAthleteName() + " is underaged." + " Age: " + person.getAge());
+						System.out.println("Duplicate Record: " + person.toString());
+					}else
+					{
+						System.out.println("Duplicate Record: " + person.toString());
+					}
 				}else
 				if(listOfAthletes.containsKey(person.getAthleteID()) == false)
 				{
@@ -60,8 +118,11 @@ public class Tester
 			}
 		}
 
-		System.out.println("List of Athletes size: " + listOfAthletes.size());
-		System.out.println("List of Athletes with Duplicate IDs: " + listOfDuplicateAthletes.size());
-		System.out.println(numberOfAthletesInjured(listOfAthletes));
+		System.out.println("List of Athletes size: " + listOfAthletes.size() + "\n");
+		System.out.println("Number of Injured Players: " + injuredPlayers + "\n");
+		System.out.println(GetAthlete(listOfAthletes, specificID) + "\n");
+		System.out.printf("%s %7.2f \n \n" ,"The total salary of 30-39 year olds on the Cincinnati Bengals team:", getSalary(listOfAthletes, "Cincinnati Bengals", 30));
+		System.out.println("List of players born in Ohio and play for the Arizona Cardinals: ");
+		teamAndState(listOfAthletes, "Arizona Cardinals", "Ohio");
 	}
 }
