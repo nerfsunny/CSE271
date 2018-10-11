c24517bf242ebc2170f4ea5713f2410a1845c807
Sunny Vaswani <nerfsunny@gmail.com>
2018-10-10 22:47:31 -0400

    Finished implementing all of the necessary methods and constructors to the class.

--- Extra Credit/EX1/E/p/Athlete.java
+++ Extra Credit/EX1/E/p/Athlete.java
@@ -63,6 +63,11 @@ public class Athlete implements Comparable<Athlete>
 		this(raf.readLine());
 	}
 
+	public Athlete(Scanner input) throws Exception
+	{
+		this(input.nextLine());
+	}
+
 	//Getter's and Setter's
 	public void setAthleteID(String id)
 	{
@@ -86,7 +91,13 @@ public class Athlete implements Comparable<Athlete>
 
 	public void setAge(int number)
 	{
-		age = number;
+		if(number > 0 && number <= 100)
+		{
+			age = number;
+		}else
+		{
+			throw new InvalidParameterException(number + " is not possible.");
+		}
 	}
 
 	public void setYearsOnTeam(int time)
@@ -157,6 +168,30 @@ public class Athlete implements Comparable<Athlete>
 		return team + " " + athleteBornIn + " " + athleteName;
 	}
 
+	public void save(RandomAccessFile raf) throws Exception
+	{
+		raf.writeChars("Athlete ID: " + athleteID);
+		raf.writeChars("Athlete Name: " + athleteName);
+		raf.writeChars("Athlete Place of Birth: " + athleteBornIn);
+		raf.writeChars("Athlete Team: " + team);
+		raf.writeInt(age);
+		raf.writeInt(yearsOnTeam);
+		raf.writeDouble(salary);
+		raf.writeBoolean(currentlyInjured);
+	}
+
+	public void save(PrintWriter pw) throws Exception
+	{
+		pw.write("Athlete ID: " + athleteID);
+		pw.write("Athlete Name: " + athleteName);
+		pw.write("Athlete Place of Birth: " + athleteBornIn);
+		pw.write("Athlete Team: " + team);
+		pw.write(Integer.toString(age));
+		pw.write(Integer.toString(yearsOnTeam));
+		pw.write(Double.toString(salary));
+		pw.write(Boolean.toString(currentlyInjured));
+	}
+
 	//Override Methods
 	@Override
 	public boolean equals(Object n)
@@ -188,12 +223,13 @@ public class Athlete implements Comparable<Athlete>
 	@Override
 	public String toString()
 	{
-		return athleteName + " " +
-			   athleteBornIn + " " +
-			   team + " " +
-			   age + " " +
-			   yearsOnTeam + " " +
-			   salary + " " +
-			   currentlyInjured;
+		return "Athlete ID: " + athleteID + "\n" +
+			   "Athlete Name: " + athleteName + "\n" +
+			   "Athlete Place of Birth: " + athleteBornIn + "\n" +
+			   "Athlete Team: " + team + "\n" +
+			   "Athlete Age: " + age + "\n" +
+			   "Athlete Tenure: " + yearsOnTeam + "\n" +
+			   "Athlete Salary: " + salary + "\n" +
+			   "Athlete Injury Status: " + currentlyInjured + "\n";
 	}
 }
