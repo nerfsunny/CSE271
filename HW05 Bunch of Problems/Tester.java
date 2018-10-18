91fbcd966179f51a3db9a37546d20f9fc666c9ba
Sunny Vaswani <nerfsunny@gmail.com>
2018-10-17 20:42:46 -0400

    Updated Tester

--- HW05 Bunch of Problems/Tester.java
+++ HW05 Bunch of Problems/Tester.java
@@ -1,10 +1,28 @@
+import java.util.*;
 
 public class Tester 
 {
 	public static void main(String args[])
-	{	SodaCan can = new SodaCan(3, 4);
-		System.out.println(can.getSurfaceArea());
-		System.out.println(can.getVolume());
+	{	
+		//======================> 8.5
+		SodaCan can = new SodaCan(3, 4);
+		System.out.println("Soda Can Surface Area: " + can.getSurfaceArea());
+		System.out.println("Soda Can Volume: " + can.getVolume());
+		System.out.println("");
+		
+		//======================> 8.7
+		Student stu1 = new Student("Sunny");
+		
+		for(int i = 0; i < 10; i++)
+		{	
+			Random number = new Random();
+			stu1.addQuiz(number.nextInt(100) + 1);
+		}
+		
+		System.out.println(stu1.getName() + "'s Total Number of Quizzes: " + stu1.getNumberOfQuizzes());
+		System.out.println(stu1.getName() + "'s Total Quiz Score: " + stu1.getTotalScore());
+		System.out.println(stu1.getName() + "'s Avergae Quiz Score: " + stu1.getAverageScore());
 	}
 	
 }
+
